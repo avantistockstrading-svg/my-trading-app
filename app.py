5130cc107144f3eb5b2646bb1a94156cd26e4542
avantistockstrading-svg <avantistockstrading@gmail.com>
2026-05-14 00:16:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import pandas_ta as ta
 import plotly.graph_objects as go
-from SmartApi import SmartConnect
+from smartapi.smartConnect import SmartConnect
 import pyotp
 from datetime import datetime, timedelta
 import time
